ead74e4e9daff6afddb03930ba781b4a479dbcb8
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-16 23:54:40 -0800

    code cleanup

--- Evadare/Source/Evadare/OpenDoor.cpp
+++ Evadare/Source/Evadare/OpenDoor.cpp
@@ -1,7 +1,4 @@
-// Cop
-
 #include "OpenDoor.h"
-
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -9,10 +6,7 @@ UOpenDoor::UOpenDoor()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UOpenDoor::BeginPlay()
@@ -20,16 +14,11 @@ void UOpenDoor::BeginPlay()
 	Super::BeginPlay();
 
 	// Find the owning Actor
-
 	// Create a rotator
-
 	// Set the door rotation
 
 	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
-
-	
 }
-
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -38,4 +27,3 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 
 	// ...
 }
-
